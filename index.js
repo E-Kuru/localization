be813bcbf5cba3459027384a90d306c2a200ce78
E-Kuru <evandalleaublop@gmail.com>
2021-12-08 11:36:04 +0100

    Exo done excepted img

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express")
 const app = express()
+const langage = require('./translation.json')
+
 
 const engine = require("express-handlebars").engine
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
 
 app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req,res) => {
+    res.render('home',{
+        layout : 'main'
+    })
+})
+
+app.get('/:lang?', (req,res) => {
+    const {lang} = req.params
+    if(lang === 'fr' || undefined){
+        res.render('trad',{
+            layout : 'main',
+            title : langage.fr.title,
+            pageTitle : langage.fr.pageTitle
+        })
+    }
+    else if (lang === 'en'){
+        res.render('trad',{
+            layout : 'main',
+            title : langage.en.title,
+            pageTitle : langage.en.pageTitle
+        })
+    }
+    else{
+        res.render('trad',{
+            layout : 'main',
+            title : langage.es.title,
+            pageTitle : langage.es.pageTitle
+        })
+
+    }
+})
 
 const port = 5000
 app.listen(port, () => {
